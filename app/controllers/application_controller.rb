9ba0f8db982c6c729cc72b4be9585ea2e8e367b5
bshap93 <benihana858@gmail.com>
2016-07-17 15:44:09 -0600

    Login routes in main controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,80 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+
+  configure do
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    #your code here
+    user = User.new(:username => params[:username], :password => params[:password], :balance => 0.0)
+    if user.save && !user.username.empty?
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
+
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    ##your code here
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get "/success" do
+    if logged_in?
+      erb :success
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
